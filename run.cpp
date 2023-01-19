c92bf7cb24a2fa059e27b8554821dd75e10f7df2
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2023-01-19 20:36:41 +0530

    Create run.cpp

---
+++ run.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int x,temp;
+        cin>>x;
+        temp=x*10;
+        cout<<temp<<endl;
+    }
+	// your code goes here
+	return 0;
+}
